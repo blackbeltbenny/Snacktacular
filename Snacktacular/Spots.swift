78d0f95ad626ba2b49fac57f65478089dc5b4ad7
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-11-02 16:49:26 -0400

    9.2, updating Snacktacular UI

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Ben Meisenzahl on 11/2/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots{
+    var spotArray = [Spot]()
+}
