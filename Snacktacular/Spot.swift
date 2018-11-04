49d431a690bd54072dfb3bbe952780c79626a0a7
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-11-04 11:25:30 -0500

    9.7, adding a mapView

--- Snacktacular/Spot.swift
+++ Snacktacular/Spot.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreLocation
 import Firebase
+import MapKit
 
-class Spot {
+class Spot: NSObject, MKAnnotation {
     var name: String
     var address: String
     var coordinate: CLLocationCoordinate2D
@@ -24,6 +25,12 @@ class Spot {
     }
     var longitude: CLLocationDegrees{
         return coordinate.longitude
+    }
+    var title: String?{
+        return name
+    }
+    var subtitle: String?{
+        return address
     }
     var dictionary: [String: Any]{
         return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
@@ -39,7 +46,7 @@ class Spot {
         self.documentID = documentID
     }
     
-    convenience init() {
+    convenience override init() {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
     }
     
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GooglePlaces
+import MapKit
 
 class SpotDetailViewController: UIViewController {
     
@@ -16,21 +17,35 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var averageRatingLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
+    
     var spot: Spot!
+    let regionDistance: CLLocationDistance = 750 //750 meters or about a half mile
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //mapView.delegate = self
+        
         if spot == nil{
             spot = Spot()
         }
-        nameField.text = spot.name
-        addressField.text = spot.address
+
+        let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        mapView.setRegion(region, animated: true)
+        updateUserInterface()
     }
     
     func updateUserInterface(){
         nameField.text = spot.name
         addressField.text = spot.address
+        updateMap()
+    }
+    
+    func updateMap(){
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(spot)
+        mapView.setCenter(spot.coordinate, animated: true)
     }
     
     func leaveViewController(){
